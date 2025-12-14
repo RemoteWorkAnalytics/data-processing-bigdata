99f3434993cb04dc27fd7fb87ac05cb0407464a9
aisha saleh <aisha.mohd.slaeh@gmail.com>
2025-12-14 09:16:30 +0200

    Move Kafka test apps to src/test and update docker compose

--- stream-processiing/src/test/scala/processing/TestConsumer.scala
+++ stream-processiing/src/test/scala/processing/TestConsumer.scala
@@ -12,9 +12,10 @@ object TestConsumer extends App {
   props.put("group.id", "test-group")
   props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
   props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("auto.offset.reset", "earliest")
 
   val consumer = new KafkaConsumer[String, String](props)
-  consumer.subscribe(Arrays.asList("test-topic"))
+  consumer.subscribe(Arrays.asList("validated-data-topic"))
 
   println("Waiting for messages...")
 
--- stream-processiing/src/test/scala/processing/TestProducer.scala
+++ stream-processiing/src/test/scala/processing/TestProducer.scala
@@ -11,7 +11,7 @@ object TestProducer extends App {
 
   val producer = new KafkaProducer[String, String](props)
 
-  val record = new ProducerRecord[String, String]("test-topic", "Hello Kafka!")
+  val record = new ProducerRecord[String, String]("validated-data-topic", "Hello Kafka!")
   producer.send(record)
   println("Message sent successfully.")
 
